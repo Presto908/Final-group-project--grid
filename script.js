f15224a215678a2ca0efee80b417ebbf247bb676
Suliman71 <Obeids@student.douglascollege.ca>
2024-07-31 10:34:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Smooth scroll for navigation links
-  document.querySelectorAll('nav ul li a').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-          e.preventDefault();
-          document.querySelector(this.getAttribute('href')).scrollIntoView({
-              behavior: 'smooth'
-          });
-      });
-  });
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    // Smooth scroll for single click on navigation links
+    navLinks.forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Navigate to the href on double click
+    navLinks.forEach(link => {
+        link.addEventListener("dblclick", function(event) {
+            event.preventDefault();
+            window.location.href = this.href;
+        });
+    });
 });
